6b5bb8858227bb6c57107ed1048a0e069e04997e
Lickitysplitted <services.cjh@gmail.com>
2023-09-12 14:12:03 -0500

    final ad search script

--- ps/adgroupsearch.ps1
+++ ps/adgroupsearch.ps1
@@ -1,11 +1,14 @@
-<# This script will take in a list of names/terms and wildcard search AD and output multiple properties, including members, into a csv. #>
-param ($file)
-param ($csv)
-
-$file = Get-Content "$file"
+<# This script will take in a list of names/terms and wildcard search AD of groups and output multiple properties, including members, into a csv. #>
+param(
+    [Parameter()]
+    [string] $file,
+    [Parameter()]
+    [string] $csv
+)
 function Search-ADGroups{
-    foreach($line in $file){
-        $result = Get-ADGroup -Properties Members -Filter "Name -like '*$line*'" | Select-Object -Property Name,SamAccountName,DistinguishedName,GroupCategory,GroupScope,ObjectClass,Members
-        $result
+    foreach($line in Get-Content $file){
+        Get-ADGroup -Properties Members -Filter "Name -like '*$line*'" | Select-Object -Property Name,SamAccountName,DistinguishedName,GroupCategory,GroupScope,ObjectClass,@{name="Members";expression={$_.Members -join "";""}}
     }
 }
+
+Search-ADGroups | Export-Csv -Path "$csv" -NoTypeInformation
