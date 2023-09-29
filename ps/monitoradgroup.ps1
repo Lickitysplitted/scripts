d59cfce0b18cd4d337e87a3f6f890ed2c1d2533a
Lickitysplitted <services.cjh@gmail.com>
2023-09-29 10:18:26 -0500

    updated message and added elapsed time tracker

--- ps/monitoradgroup.ps1
+++ ps/monitoradgroup.ps1
@@ -8,13 +8,16 @@ param(
 )
 
 function Watch-ADGroupMembers{
+    $starttime = Get-Date
     $members = Get-ADGroupMember -Identity $identity
     $membernames = $members | Select-Object Name
     $startstate = $members | Measure-Object
     $monitorstate = $startstate
     Write-Host $membernames.Name
     while($monitorstate.Count -eq $startstate.Count){
-        Write-Host "Current member Count is",$startstate.Count,". No change yet. Sleeping for ",$interval," seconds"
+        $currenttime = Get-Date
+        $elapsedtime = New-TimeSpan -Start $starttime -End $currenttime
+        Write-Host "Member Count:",$startstate.Count,"Elapsed Time:",$elapsedtime,"Sleep:",$interval,"seconds"
         Start-Sleep $interval
         $monitorstate = Get-ADGroupMember -Identity $identity | Measure-Object
     }
