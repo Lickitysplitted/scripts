725a857b4fbca087bed0803e5f8944a1092e5629
Lickitysplitted <services.cjh@gmail.com>
2024-10-17 15:36:45 -0500

    update whereami

--- ps/whereami.ps1
+++ ps/whereami.ps1
@@ -1,36 +1,48 @@
-<# this script will use various methods to determine external ip addresses#>
+<# 
+.SYNOPSIS
+    This script determines the system's external IP address using DNS and HTTP methods.
+.DESCRIPTION
+    The script queries various DNS and HTTP services to obtain the external IP address of the system.
+.NOTES
+    Author: [Your Name]
+    Date: [Date]
+.EXAMPLE
+    .\whereami.ps1
+#>
+
 $dnsipsvc = @(("OpenDNS", "myip.opendns.com", "resolver1.opendns.com"),("Google", "o-o.myaddr.l.google.com", "ns1.google.com"))
 $httpipsvc = "checkip.amazonaws.com", "ifconfig.me", "icanhazip.com", "ipecho.net/plain"
-Write-Host "DNS Methods`nForced"
-foreach ($svc in $dnsipsvc){
-    if ($svc[0] -eq "Google"){
-        $answer = Resolve-DnsName -Name $svc[1] -Type TXT -Server $svc[2]
-        Write-host $svc[0], ": ", $answer.Strings
-    }
-    else {
-        $answer = Resolve-DnsName -Name $svc[1] -Server $svc[2]
-        Write-host $svc[0], ": ", $answer.IPAddress
-    }
-}
-Write-Host "`nNative"
-foreach ($svc in $dnsipsvc){
-    if ($svc[0] -eq "Google"){
-        $answer = Resolve-DnsName -Name $svc[1] -Type TXT
-        Write-host "Google: ", $answer.Strings
-    }
-    elseif ($svc[0] -eq "OpenDNS"){
-        $answer = Resolve-DnsName -Name $svc[1]
-        Write-host "OpenDNS: ", $answer.IPAddress
+
+# Function to query DNS for external IP
+function Get-DNSExternalIP {
+    param ($svcName, $queryName, $server)
+    try {
+        $answer = Resolve-DnsName -Name $queryName -Server $server
+        Write-Host "'$svcName': $($answer.IPAddress)"
+    } catch {
+        Write-Warning "Error resolving '$svcName': $_"
     }
 }
+
+# Function to query HTTP services for external IP
+function Get-HTTPExternalIP {
+    param ($url)
+    try {
+        $resp = Invoke-WebRequest -Uri $url -TimeoutSec 5
+        Write-Host "$url: $($resp.Content)"
+    } catch {
+        Write-Warning "Error fetching from $url: $_"
+    }
+}
+
+# Query DNS methods
+Write-Host "DNS Methods"
+foreach ($svc in $dnsipsvc) {
+    Get-DNSExternalIP $svc[0] $svc[1] $svc[2]
+}
+
+# Query HTTP methods
 Write-Host "`nHTTP Methods"
-foreach ($site in $httpipsvc){
-    if ($site -eq "checkip.amazonaws.com"){
-        $resp = [System.Text.Encoding]::ASCII.GetString((Invoke-WebRequest -Uri $site).Content)
-        Write-Host $site, ": ", $resp
-    }
-    else {
-        $resp = Invoke-WebRequest -Uri $site -Headers @{"Cache-Control"="no-cache"}
-        Write-Host $site, ": ", $resp.Content
-    }
+foreach ($site in $httpipsvc) {
+    Get-HTTPExternalIP $site
 }
